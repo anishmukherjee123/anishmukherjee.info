357e74b1313b3d773efc7823f3182592b304ae3e
Anish Mukherjee <mukherjee.an@northeastern.edu>
2023-06-01 12:39:28 -0400

    Added pic and basic animations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,16 @@ import "bootstrap/dist/css/bootstrap.css";
 
 export function Footer(): JSX.Element {
     return (
-        <footer className="footer border-t py-3 bg-light">
-            <div className="container">
-                <div className="row justify-content-end">
-                    <div className="col-4 mb-0 text-muted">
-                        <span className="">
-                            This site was built from scratch in React!
-                        </span>
-                    </div>
-                    <div className="col-2 mb-0 text-muted">
-                        <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-                            href="https://github.com/anishmukherjee123/anishmukherjee.info">
-                            Repo available here.
-                        </a>
-                    </div>
-                </div>
+        <footer className="footer border-t py-3 bg-light fixed-bottom">
+            <div className="container-fluid text-center text-muted fst-italic">
+                <span className="">
+                    This site was built from scratch in React!
+                </span>
+                &nbsp;
+                <a 
+                    href="https://github.com/anishmukherjee123/anishmukherjee.info">
+                    Repo available here.
+                </a>
             </div>
         </footer>
     );
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,23 +2,29 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
+import picOfMe from "../images/me.jpg";
+import 'animate.css';
 
 function HomePage(): JSX.Element {
   return (
     <div className=".bg-light.bg-gradient">
       <Navbar/>
-      <div className="container-fluid m-4">
+      <div className="container-fluid my-3">
         <div className="row align-items-start">
           <div className="col-2">
-            Image here
+            <img src={picOfMe} alt="Pic of me!" className="float-start img-fluid animate__animated animate__fadeInLeft"/>
           </div>
           <div className="col-7">
+            <div className="animate__animated animate__fadeInUp">
+              <p className="h1 fw-bolder font-monospace">Welcome!</p>
+              <hr className="border border-primary border-3 opacity-75"/>
+            </div>
             Welcome to my home page! As you can see it's a work in progress ;)
             <br></br>
-            But just in case you still want to get in touch, you can reach me at me@anishmukherjee.info!
+            But just in case you want to get in touch, you can reach me at <span className="fw-bold">me@anishmukherjee.info</span>!
           </div>
           <div className="col-2">
-            Column here
+            WIP!
           </div>
         </div>
       </div>
