730f3f623b6708d8225d2cef63262c176806b26c
Anish Mukherjee <mukherjee.an@northeastern.edu>
2024-04-07 19:29:09 -0400

    Working on customizing astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'anishmukherjee.info';
+export const SITE_DESCRIPTION = "Anish Mukherjee's personal website";
